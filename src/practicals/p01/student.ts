1f0ab80eaf140f1e4a0843d51a1b4e7bf2ce145e
surawitpakeepol <surawitpakeepol@gmail.com>
2026-02-16 09:36:29 +0700

    aaaa

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,13 @@
-export class Student {}
+export class Student {
+    firstname: string
+    lastname: string
+
+    constructor(firsname: string ,lastname: string){
+        this.firstname = firsname = 'John';
+        this.lastname = lastname = 'Doe';
+    }
+    printName(){
+        console.log(`${this.firstname} ${this.lastname}`);
+    }
+    
+}
