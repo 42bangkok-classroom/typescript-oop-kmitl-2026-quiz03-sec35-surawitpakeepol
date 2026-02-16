5464275fe5b04fb4d893148843fdb65fd30e1575
surawitpakeepol <surawitpakeepol@gmail.com>
2026-02-16 10:00:12 +0700

    aaaa

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -5,14 +5,11 @@ export class Person {
     private age :number = 25;
     getFullName() {
         return `${this.firstname} ${this.lastname}`;
-
     }
     setAge(age : number ) {
         if(!isNaN(age)){
-
             this.age = age;
-        }
-        
+        }  
     }
     getAge(): number {
         return this.age;
